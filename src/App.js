1f38be06818cae66e47fd54b2a7a52ad9c7153f7
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2024-07-11 01:33:39 +0530

    Small Changes

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ import {
   Route,
   Outlet,
 } from "react-router-dom";
-import Home from "./pages/Home"
+import Home from "./pages/Home";
 import { productsData } from "./api/api";
-import SignIn from "./pages/SignIn"
+import SignIn from "./pages/SignIn";
+import Registration from "./pages/Registration";
 
 const Layout = () => {
   return (
@@ -26,9 +27,12 @@ const Layout = () => {
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
+      <Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} loader={productsData}></Route>
+        </Route>
         <Route path="/signin" element={<SignIn />}></Route>
-        <Route index element={<Home />} loader={productsData}></Route>
+        <Route path="/registration" element={<Registration />}></Route>
       </Route>
     )
   );
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -5,21 +5,33 @@ import darkLogo from "../assets/darkLogo.png";
 import { ArrowRight } from "@mui/icons-material";
 import { Link } from "react-router-dom";
 
-const SignIn = () => {
+const Registration = () => {
+  const [clientName, setClientName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
 
+  // Error Message Start
+  const [errClientName, setErrClientName] = useState("");
   const [errEmail, setErrEmail] = useState("");
   const [errPassword, setErrPassword] = useState("");
+  const [errCPassword, setErrCPassword] = useState("");
 
+  const handleName = (e) => {
+    setClientName(e.target.value);
+    setErrClientName("");
+  };
   const handleEmail = (e) => {
     setEmail(e.target.value);
     setErrEmail("");
   };
-
   const handlePassword = (e) => {
     setPassword(e.target.value);
     setErrPassword("");
+  };
+  const handleCPassword = (e) => {
+    setCPassword(e.target.value);
+    setErrCPassword("");
   };
 
   const emailValidation = (email) => {
@@ -30,6 +42,9 @@ const SignIn = () => {
 
   const handleRegisteration = (e) => {
     e.preventDefault();
+    if (!clientName) {
+      setErrClientName("Please enter your name");
+    }
     if (!email) {
       setErrEmail("Please enter your email");
     } else {
@@ -44,25 +59,50 @@ const SignIn = () => {
         setErrPassword("Password must be of 6 characters");
       }
     }
-    if (email && emailValidation(email) && password && password.length >= 6) {
-      console.log(email, password);
-      setEmail("");
-      setPassword("");
+    if (!cPassword) {
+      setErrCPassword("Confirm your password");
+    } else {
+      if (password !== cPassword) {
+        setErrCPassword("Password does not match");
+      }
+    }
+    if(clientName && email && emailValidation(email) && password && password.length >= 6 && cPassword && cPassword === password){
+        console.log(clientName, email, password);
+        setClientName("");
+        setEmail("");
+        setPassword("");
+        setCPassword("");
     }
   };
-
   return (
     <div className="w-full">
       <div className="w-full bg-white pb-10">
-        <form className="w-[370px] mx-auto flex flex-col items-center">
+        <form className="w-[370px] mx-auto flex flex-col items-center bg-zinc-50">
           <img className="w-32" src={darkLogo} alt="darkLogo" />
-          <div className="w-full border border-zinc-200 p-6 bg-zinc-50">
-            <h2 className="font-titleFont text-3xl font-medium mb-4">SignIn</h2>
+          <div className="w-full border border-zinc-200 p-6">
+            <h2 className="font-titleFont text-3xl font-medium mb-4">
+              Create Account
+            </h2>
             <div className="flex flex-col gap-3">
               <div className="flex flex-col gap-2">
-                <p className="text-sm font-medium">
-                  Email or Mobile Phone number
-                </p>
+                <p className="text-sm font-medium">Your Name</p>
+                <input
+                  onChange={handleName}
+                  value={clientName}
+                  className="w-full py-1 border border-zinc-400 px-2 text-base rounded-sm outline-none focus-within:border-[#e77600] focus-within:shadow-amazonInput"
+                  type="text"
+                />
+                {errClientName && (
+                  <p className="text-xs text-red-600 font-semibold tracking-wide flex items-center gap-2 -mt-1.5">
+                    <span className="italic font-titleFont font-extrabold">
+                      !
+                    </span>
+                    {errClientName}
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-sm font-medium">Email or Phone number</p>
                 <input
                   onChange={handleEmail}
                   value={email}
@@ -81,9 +121,9 @@ const SignIn = () => {
               <div className="flex flex-col gap-2">
                 <p className="text-sm font-medium">Password</p>
                 <input
-                  value={password}
                   onChange={handlePassword}
-                  className="w-full lowercase py-1 border border-zinc-400 px-2 text-base rounded-sm outline-none focus-within:border-[#e77600] focus-within:shadow-amazonInput"
+                  value={password}
+                  className="w-full py-1 border border-zinc-400 px-2 text-base rounded-sm outline-none focus-within:border-[#e77600] focus-within:shadow-amazonInput"
                   type="password"
                 />
                 {errPassword && (
@@ -94,6 +134,26 @@ const SignIn = () => {
                     {errPassword}
                   </p>
                 )}
+              </div>
+              <div className="flex flex-col gap-2">
+                <p className="text-sm font-medium">Re-Enter Password</p>
+                <input
+                  onChange={handleCPassword}
+                  value={cPassword}
+                  className="w-full py-1 border border-zinc-400 px-2 text-base rounded-sm outline-none focus-within:border-[#e77600] focus-within:shadow-amazonInput"
+                  type="password"
+                />
+                {errCPassword && (
+                  <p className="text-xs text-red-600 font-semibold tracking-wide flex items-center gap-2 -mt-1.5">
+                    <span className="italic font-titleFont font-extrabold">
+                      !
+                    </span>
+                    {errCPassword}
+                  </p>
+                )}
+                <p className="text-xs text-gray-600">
+                  Password must be of 6 characters
+                </p>
               </div>
               <button
                 onClick={handleRegisteration}
@@ -107,23 +167,26 @@ const SignIn = () => {
               <span className="text-blue-600">Conditions of Use </span> and
               <span className="text-blue-600"> Privacy Notice</span>
             </p>
-            <p className="text-xs text-gray-600 mt-4 cursor-pointer group">
-              <ArrowRight />{" "}
-              <span className="text-blue-600 group-hover:text-orange-700 group-hover:underline underline-offset-1">
-                Need Help?
-              </span>
-            </p>
+            <div>
+              <p className="text-sm text-black leading-4 mt-4">
+                Already have an account?{" "}
+                <Link className="w-full" to="/signin">
+                  <span className="text-blue-600 text-sm hover:text-orange-600 hover:underline underline-offset-1 cursor-pointer duration-100">
+                    Sign in
+                    <span className="text-blue-600">
+                      <ArrowRight />
+                    </span>
+                  </span>
+                </Link>
+              </p>
+              <p className="text-sm text-black -mt-2">
+                Buying for work?{" "}
+                <span className="text-blue-600 text-xs hover:text-orange-600 hover:underline underline-offset-1 cursor-pointer duration-100">
+                  Create a free business account
+                </span>
+              </p>
+            </div>
           </div>
-          <p className="w-full text-xs text-gray-600 mt-4 flex items-center">
-            <span className="w-1/3 h-[1px] bg-zinc-400 inline-flex"></span>
-            <span className="w-1/3 text-center">New to Amazon?</span>
-            <span className="w-1/3 h-[1px] bg-zinc-400 inline-flex"></span>
-          </p>
-          <Link className="w-full" to="/registration">
-            <button className="w-full py-1.5 mt-4 text-sm font-normal rounded-sm bg-gradient-to-t from-slate-200 to-slate-100 hover:bg-gradient-to-b border border-zinc-400 active:border-yellow-800 active:shadow-amazonInput">
-              Create Your Amazon Account
-            </button>
-          </Link>
         </form>
       </div>
       <div className="w-full bg-gradient-to-t from-white via-white to-zinc-200 flex flex-col gap-4 items-center justify-center py-10">
@@ -146,4 +209,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default Registration;
