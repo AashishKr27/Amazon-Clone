67947affa3e05fb5e29a8b4ced5685ec71a47a78
AashishKr27 <ashish.21gcebds063@galgotiacollege.edu>
2024-07-11 19:29:40 +0530

    commit

--- src/components/Header/HeaderBottom.js
+++ src/components/Header/HeaderBottom.js
@@ -5,18 +5,18 @@ import { Menu } from "@mui/icons-material";
 import { Close } from "@mui/icons-material";
 import { AccountCircle } from "@mui/icons-material";
 import SideNavContent from "./SideNavContent";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 
 const HeaderBottom = () => {
-    const ref = useRef()
+  const ref = useRef();
   const [sidebar, setSidebar] = useState(false);
-  useEffect(()=>{
-    document.body.addEventListener("click", (e)=>{
-        if(e.target.contains(ref.current)){
-            setSidebar(false)
-        }
-    })
-  }, [ref, sidebar])
+  useEffect(() => {
+    document.body.addEventListener("click", (e) => {
+      if (e.target.contains(ref.current)) {
+        setSidebar(false);
+      }
+    });
+  }, [ref, sidebar]);
   return (
     <div className="w-full px-4 h-[36px] bg-amazon_light text-white">
       <ul className="flex items-center gap-2 text-sm tracking-wide">
@@ -46,7 +46,13 @@ const HeaderBottom = () => {
       {sidebar && (
         <div className="w-full h-screen text-black fixed top-0 left-0 bg-amazon_blue bg-opacity-50">
           <div className="w-full h-full relative">
-            <motion.div ref={ref} initial={{x:-500, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:.5}} className="md:w-[350px] w-[80%] h-full bg-white border border-black">
+            <motion.div
+              ref={ref}
+              initial={{ x: -500, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 0.5 }}
+              className="md:w-[350px] w-[80%] h-full bg-white border border-black"
+            >
               <div className="w-full bg-amazon_light text-white py-2 px-6 flex itesm-center gap-4">
                 <AccountCircle />
                 <h3 className="font-titleFont font-bold text-lg tracking-wide">
@@ -77,7 +83,12 @@ const HeaderBottom = () => {
                 two="Customer Service"
                 three="Contact Us"
               />
-            <span onClick={()=>setSidebar(false)} className="cursor-pointer absolute top-0 md:left-[360px] left-[82%] w-10 h-10 text-black flex items-center justify-center border bg-gray-200 hover:bg-red-600 hover:text-white duration-300"><Close/></span>
+              <span
+                onClick={() => setSidebar(false)}
+                className="cursor-pointer absolute top-0 md:left-[360px] left-[82%] w-10 h-10 text-black flex items-center justify-center border bg-gray-200 hover:bg-red-600 hover:text-white duration-300"
+              >
+                <Close />
+              </span>
             </motion.div>
           </div>
         </div>
